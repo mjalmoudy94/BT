40b862b98c96bdd84184fcea82a1e96e138b35d3
mjalmoudy94 <m.jalmoudy94@gmail.com>
2020-02-17 17:26:58 +0330

    add Tbot

---
+++ BT/Class/TBot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace BT
+{
+    public static class TBot
+    {
+        private static List<string> UsersID = new List<string>
+        {
+            "78649634"
+            //,"485736315"
+        };
+        private static string Token = "992031045:AAEYQvxujIKOlMHWesz_JzoYKCBzq1UoXh4";
+        //
+        public static TelegramBotClient telegram = new TelegramBotClient(Token);
+        //
+        //
+        //
+        public static void broadcastMessage(string MSG)
+        {
+            foreach (var userID in UsersID)
+            {
+                telegram.SendTextMessageAsync(new ChatId(userID), MSG);
+            }
+        }
+    }
+}
---
+++ BT/Class/Trader.cs
@@ -0,0 +1,54 @@
+﻿using Binance.Net;
+using Binance.Net.Objects;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BT
+{
+    public static class Trader
+    {
+
+        //
+        public static Task BT = new Task(() =>
+        {
+            init();
+            //
+            while (true)
+            {
+                //TBot.broadcastMessage("test");
+                System.Threading.Thread.Sleep(5000);
+            }
+        });
+        //
+        static void init()
+        {
+            //
+            using (var client = new BinanceSocketClient())
+            {
+                client.SubscribeToSymbolTickerUpdatesAsync("BCHUSDT", (data) =>
+                {
+                    TickerUpdate(data);
+                });
+
+                client.SubscribeToKlineUpdatesAsync("BCHUSDT", KlineInterval.OneMinute , (data) =>
+                {
+                    TBot.broadcastMessage(data.Data.Volume.ToString(new CultureInfo("en-US")));
+                });
+            }
+        }
+        //
+        //
+        // New Price Change Update
+        static void TickerUpdate(BinanceStreamTick data)
+        {
+            TBot.broadcastMessage(data.WeightedAverage.ToString("C", new CultureInfo("en-US")));
+        }
+        //
+
+    }
+}
