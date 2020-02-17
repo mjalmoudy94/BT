19bbca38b019ba0cd5c214c9727728aaa6e11461
mjalmoudy94 <m.jalmoudy94@gmail.com>
2020-02-17 13:49:56 +0330

    update

--- BT/Global.asax.cs
+++ BT/Global.asax.cs
@@ -16,6 +16,7 @@ namespace BT
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Trader.BT.Start();
         }
     }
 }
---
+++ BT/Trader/Trader.cs
@@ -0,0 +1,53 @@
+﻿using Binance.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BT
+{
+    public static class Trader
+    {
+
+        //
+        public static Task BT = new Task(() => {
+            while (true)
+            {
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        });
+        //
+        static void init()
+        {
+			using (var client = new BinanceSocketClient())
+			{
+				var successDepth = client.SubscribeToDepthStream("bnbbtc", (data) =>
+				{
+					// handle data
+				});
+				var successTrades = client.SubscribeToTradesStream("bnbbtc", (data) =>
+				{
+					// handle data
+				});
+				var successKline = client.SubscribeToKlineStream("bnbbtc", KlineInterval.OneMinute, (data) =>
+				{
+					// handle data
+				});
+				var successSymbol = client.SubscribeToSymbolTicker("bnbbtc", (data) =>
+				{
+					// handle data
+				});
+				var successSymbols = client.SubscribeToAllSymbolTicker((data) =>
+				{
+					// handle data
+				});
+				var successOrderBook = client.SubscribeToPartialBookDepthStream("bnbbtc", 10, (data) =>
+				{
+					// handle data
+				});
+			}
+		}
+    }
+}
